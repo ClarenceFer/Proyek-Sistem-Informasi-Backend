e263aa04f98a8ee915ed587775fc4caa9cdf6eab
Clarence <ferdinandclarence5@gmail.com>
2025-10-03 08:23:40 +0700

    Menambahkan link yang bisa akses backend ke-4

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,18 +2,18 @@ const { verifySignup } = require("../middlewares");
 const controller = require("../controllers/auth.controller");
 const { authJwt } = require("../middlewares");
 
-module.exports = function(app) {
-  // Middleware CORS untuk mengizinkan frontend Vercel
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://www.sibaso.site"); // frontend domain
-    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    next();
-  });
+module.exports = function (app) {
+  const controller = require("../controllers/auth.controller");
+  const { authJwt, verifySignup } = require("../middlewares");
+  const express = require("express");
+
+  const router = express.Router();
 
   // Endpoint login
-  app.post("/api/auth/signin", controller.signin);
+  router.post("/signin", controller.signin);
 
   // Debug endpoint untuk cek database
-  app.get("/api/auth/debug/check-database", controller.checkDatabase);
-};
+  router.get("/debug/check-database", controller.checkDatabase);
+
+  module.exports = router;
+}
