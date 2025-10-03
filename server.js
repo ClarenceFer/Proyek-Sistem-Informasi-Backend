aba7c7b948ce239ae768b0955ad0bcc42280eeb3
Clarence <ferdinandclarence5@gmail.com>
2025-10-03 08:07:26 +0700

    Menambahkan link yang bisa akses backend

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Configure CORS
 const corsOptions = {
-    origin: ["https://www.sibaso.site"] // URL frontend React
+    origin: ["https://www.sibaso.site","https://testing-prosi.vercel.app"] // URL frontend React
 };
 app.use(cors(corsOptions));
 
